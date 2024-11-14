b15605dede051d9f346c2ebad5195a53c9eecef9
clintSanch <clintonodira@gmail.com>
2024-11-14 13:06:50 +0300

    first FE Commits

--- Generat-Election-FE/src/app/app.component.ts
+++ Generat-Election-FE/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Kenyans Decide-2027';
+  title = 'Kenya Decide-2027';
 }
